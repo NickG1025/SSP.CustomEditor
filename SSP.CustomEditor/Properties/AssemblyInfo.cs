b30c00d1c9284020fe798ce5416430679bac69da
unknown <ig31@IGCPF0D0E072.i04.local>
2012-04-27 17:57:11 +0200

    proper versioning and nuspec

--- SSP.CustomEditor/Properties/AssemblyInfo.cs
+++ SSP.CustomEditor/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SSP.CustomEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Intility AS")]
+[assembly: AssemblyCompany("Sem & Stenersen Prokom")]
 [assembly: AssemblyProduct("SSP.CustomEditor")]
-[assembly: AssemblyCopyright("Copyright © Intility AS 2012")]
+[assembly: AssemblyCopyright("Copyright © Sem & Stenersen Prokom AS 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
